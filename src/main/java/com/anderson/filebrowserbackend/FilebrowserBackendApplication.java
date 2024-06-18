27f9004f831897c9d2fd5090dcdb7baba4b3e943
srpromax <andersonbdavila00@gmail.com>
2024-06-17 19:32:47 -0500

    first commit

---
+++ src/main/java/com/anderson/filebrowserbackend/FilebrowserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.filebrowserbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilebrowserBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilebrowserBackendApplication.class, args);
+    }
+
+}
